b946fc49b14d782f8d668bce6cdf70d7414a3fc2
Chris Clark <ludachrispeed@gmail.com>
2018-11-01 10:12:40 -0400

    [fish] Add tmuxinit function

--- .functions.fish
+++ .functions.fish
@@ -4,7 +4,6 @@ function fish_prompt --description 'Defines the prompt'
     set stat $status
 
     if not set -q __fish_prompt_normal
-        # normal keyward resets foreground, background, and all formatting back to default
         set -g __fish_prompt_normal (set_color normal)
     end
 
@@ -48,6 +47,27 @@ function man --description 'Colorize man'
     set -x LESS_TERMCAP_ue (set_color normal)
     set -x LESS_TERMCAP_me (set_color normal)
     env man $argv
+end
+
+function tmuxinit --description 'Initialize tmux'
+    # https://github.com/tmux-plugins/tmux-copycat/issues/116
+    tmux unbind -n n \; unbind -n N
+
+    # Sesison: Projects
+    tmux new-session -d -s Projects
+    tmux new-window -dk -n rust -t 1
+
+    # Session: Work
+    tmux new-session -d -s Work
+    tmux new-window -dk -n local -t 1
+    tmux new-window -dk -n cloudcover -t 2
+
+    # Session: QA-AWS
+    tmux new-session -d -s QA-AWS
+    tmux new-window -dk -n chrisc -t 1
+    tmux new-window -dk -n chrisc-qa-0 -t 2
+    tmux new-window -dk -n chrisc-qa-1 -t 3
+    tmux new-window -dk -n chrisc-alt -t 4
 end
 
 function ssh_decrypt --description 'Decrypt password-protected ssh key'
