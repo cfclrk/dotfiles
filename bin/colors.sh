eba99a626d4aeea4aa81d42584ddde558db340e2
Chris Clark <cfclrk@gmail.com>
2019-02-05 20:56:24 -0500

    [bash] Add colors.sh

---
+++ bin/colors.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# Reset
+Color_Off='\[\e[0m\]'
+
+# Regular Colors
+Black='\[\e[0;30m\]'
+Red='\[\e[0;31m\]'
+Green='\[\e[0;32m\]'
+Yellow='\[\e[0;33m\]'
+Blue='\[\e[0;34m\]'
+Purple='\[\e[0;35m\]'
+Cyan='\[\e[0;36m\]'
+White='\[\e[0;37m\]'
+
+# Bold
+BBlack='\[\e[1;30m\]'
+BRed='\[\e[1;31m\]'
+BGreen='\[\e[1;32m\]'
+BYellow='\[\e[1;33m\]'
+BBlue='\[\e[1;34m\]'
+BPurple='\[\e[1;35m\]'
+BCyan='\[\e[1;36m\]'
+BWhite='\[\e[1;37m\]'
+
+# Underline
+UBlack='\[\e[4;30m\]'
+URed='\[\e[4;31m\]'
+UGreen='\[\e[4;32m\]'
+UYellow='\[\e[4;33m\]'
+UBlue='\[\e[4;34m\]'
+UPurple='\[\e[4;35m\]'
+UCyan='\[\e[4;36m\]'
+UWhite='\[\e[4;37m\]'
+
+# Background
+OnBlack='\[\e[40m\]'
+OnRed='\[\e[41m\]'
+OnGreen='\[\e[42m\]'
+OnYellow='\[\e[43m\]'
+OnBlue='\[\e[44m\]'
+OnPurple='\[\e[45m\]'
+OnCyan='\[\e[46m\]'
+OnWhite='\[\e[47m\]'
