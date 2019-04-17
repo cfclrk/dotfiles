e198a0f2a7664e73606d1a512d6e1436ee9c4fdf
Chris Clark <cfclrk@gmail.com>
2019-04-17 11:02:29 -0400

    [bash] Update export_aws function to use only 2 lines

--- .functions.sh
+++ .functions.sh
@@ -1,8 +1,13 @@
-function export_aws {
+#!/usr/bin/env bash
+
+export_aws() {
+    [[ -n "$1" ]] || {
+        echo "Error: AWS profile name is required."
+        return 1
+    }
     profile="\[$1\]"
-    creds=$(grep $profile -A 3 ~/.aws/credentials | tail -n 3 | tr "=" "\n")
+    creds=$(grep "$profile" -A 2 ~/.aws/credentials | tail -n 2 | tr "=" "\n")
     creds_array=($creds)
     export $(echo ${creds_array[0]} | tr '[:lower:]' '[:upper:]')=$(echo ${creds_array[1]})
     export $(echo ${creds_array[2]} | tr '[:lower:]' '[:upper:]')=$(echo ${creds_array[3]})
-    export $(echo ${creds_array[4]} | tr '[:lower:]' '[:upper:]')=$(echo ${creds_array[5]})
 }
