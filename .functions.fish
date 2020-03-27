2c5f4a459cbc889062657b9e37455c22a9caea5d
Chris Clark <cfclrk@gmail.com>
2020-03-27 13:27:25 -0400

    [go] Update URL to get x/tools

--- .functions.fish
+++ .functions.fish
@@ -104,7 +104,7 @@ function get_go_tools
     go get -u github.com/zmb3/gogetdoc  # Used in emacs godoc-at-point-function
     go get -u github.com/rogpeppe/godef  # Find symbol information in Go source
     go get -u github.com/mdempsky/gocode  # Editor auto completion
-    go get -u golang.org/x/tools/cmd/...  # godoc, goimports, so much more
+    go get -u golang.org/x/tools/...  # godoc, gopls, goimports, gorename, etc
 end
 
 # Python
