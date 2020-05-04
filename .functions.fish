96d59ca43f0c3da615c4ed4043ee5a91b712e20a
Chris Clark <cfclrk@gmail.com>
2020-05-04 09:44:33 -0400

    [fish] Update s function to interpolate values

--- .functions.fish
+++ .functions.fish
@@ -40,10 +40,25 @@ function fish_prompt --description 'Defines the prompt'
     "$__fish_prompt_normal"
 end
 
+# Works if env file has only key=value pairs, but will not interpolate values
+# (i.e. cannot have key=$value pairs)
+
+# function s --description "Export variables in a .env file"
+#     test -z "$argv[1]"; and echo "Error: arg1 must be a file path"; and return
+#     set envFile $argv[1]
+#     export (cat $envFile | xargs -L 1)
+# end
+
 function s --description "Export variables in a .env file"
-    test -z "$argv[1]"; and echo "Error: arg1 must be a file path"; and return
+    test -z "$argv[1]"; and echo "Error: arg1 must be a file path"; and return 1
     set envFile $argv[1]
-    export (cat $envFile | xargs -L 1)
+
+    for line in (cat $envFile)
+        if test -n $line
+            set -l v (string split -m 1 "=" -- $line | string join " ")
+            eval set -gx $v
+        end
+    end
 end
 
 function grep --description 'Override grep'
