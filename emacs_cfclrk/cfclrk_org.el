b44eeba8382cf2d8783be2b840aeb957a276f568
Chris Clark <cfclrk@gmail.com>
2021-02-22 20:29:47 -0500

    Add org publish projects

--- emacs_cfclrk/cfclrk_org.el
+++ emacs_cfclrk/cfclrk_org.el
@@ -17,7 +17,9 @@
 ;;; org-superstar
 
 (use-package org-superstar
-  :hook (org-mode . org-superstar-mode))
+  :hook (org-mode . org-superstar-mode)
+  :config
+  (setq org-hide-leading-stars t))
 
 ;;; org-tree-slide
 
@@ -29,11 +31,59 @@
 
 (require 'ox-publish)
 (setq org-publish-project-alist
-	  '(("technotes"
+	  '(("articles"
+         :recursive t
+		 :base-directory "~/Projects/technotes/articles"
+		 :publishing-directory "~/Projects/technotes/_site/articles"
+		 :publishing-function org-html-publish-to-html
+		 :html-head-include-scripts nil
+		 :html-head-include-default-style nil
+		 :auto-sitemap t)
+
+		("notes"
 		 :recursive t
-		 :base-directory "~/Projects/technotes/org/"
-		 :publishing-directory "~/Projects/technotes/html/"
-		 :publishing-function org-html-publish-to-html)))
+		 :base-directory "~/Projects/technotes/notes"
+		 :publishing-directory "~/Projects/technotes/_site/notes"
+		 :publishing-function org-html-publish-to-html
+		 :html-head-include-scripts nil
+		 :html-head-include-default-style nil
+		 :auto-sitemap t)
+
+		("cf-include"
+         :recursive t
+		 :base-directory "~/Projects/technotes/cloudformation/org"
+		 :publishing-directory "~/Projects/technotes/cloudformation/_gen"
+		 :publishing-function org-org-publish-to-org)
+
+		("cf-tangle"
+         :recursive t
+		 :base-directory "~/Projects/technotes/cloudformation/_gen"
+		 :publishing-directory "~/Projects/technotes/cloudformation/_tangle"
+		 :publishing-function org-babel-tangle-publish)
+
+		("cf-html"
+         :recursive t
+		 :base-directory "~/Projects/technotes/cloudformation/_gen"
+		 :publishing-directory "~/Projects/technotes/_site/cloudformation"
+		 :publishing-function org-html-publish-to-html
+		 :html-head-include-scripts nil
+		 :html-head-include-default-style nil
+		 :auto-sitemap t)
+
+		("static"
+		 :recursive t
+		 :base-directory "~/Projects/technotes/static"
+		 :publishing-directory "~/Projects/technotes/_site/static"
+		 :base-extension "png\\|jpg\\|gif\\|pdf\\|css"
+		 :publishing-function org-publish-attachment)
+
+		("site"
+		 :components ("articles"
+					  "notes"
+					  "cf-include"
+					  "cf-tangle"
+					  "cf-html"
+					  "static"))))
 
 ;;; org-src mode
 
@@ -53,7 +103,8 @@
   (setq org-startup-folded t
 		org-confirm-babel-evaluate nil
 		org-src-window-setup 'split-window-below
-		org-special-ctrl-a/e t)
+		org-special-ctrl-a/e t
+		org-babel-clojure-backend 'cider)
 
   ;; Note my smartparens config also pulls in 'smartparens-org
   (smartparens-mode +1)
@@ -67,6 +118,16 @@
 	 (gnuplot . t)
 	 (python . t)
      (shell . t)))
+
+  ;; Set babel default headrs
+  (add-to-list 'org-babel-default-header-args '(:noweb . "yes"))
+  (add-to-list 'org-babel-default-header-args '(:eval . "never-export"))
+
+  ;; Ensure incorrect shell blocks fail nicely
+  (add-to-list 'org-babel-default-header-args:sh
+               '(:prologue . "set -eu -o pipefail"))
+  (add-to-list 'org-babel-default-header-args:bash
+               '(:prologue . "set -eu -o pipefail"))
 
   ;; HTML exporting
   (setq org-html-doctype "html5"
