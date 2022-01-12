381ca18026ab57a4eaa59fc1e1b90fcba88846ce
Chris Clark <cfclrk@gmail.com>
2022-01-12 08:46:18 -0500

    [dotfiles] Start adding lang files

---
+++ langs/rust.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Install rustup
+
+# Install components
+rustup component add clippy
+
+# Add wasm component
+
+# Important rust programs
+cargo install cargo-edit
+
+# Install rust-analyzer (LSP server)
+brew install rust-analyzer
