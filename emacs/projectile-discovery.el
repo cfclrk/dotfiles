4efc2cc072527e1cab2e72361ea75183ccb7e781
Chris Clark <cfclrk@gmail.com>
2022-02-09 14:42:39 -0500

    Update projectile-discovery.el

--- emacs/projectile-discovery.el
+++ emacs/projectile-discovery.el
@@ -16,32 +16,52 @@
 (require 'projectile)
 (require 'f)
 
-(setq my-project-root-files
-      (-concat '("go.mod" "requirements.txt")
-               projectile-project-root-files-bottom-up
-               projectile-project-root-files))
+(setq
+ my-project-root-files
+ (-concat
+  projectile-project-root-files-bottom-up
+  projectile-project-root-files))
+
+;; Python
+(add-to-list
+ 'my-project-root-files
+ "requirements.txt")
+
+;; Go
+(add-to-list
+ 'my-project-root-files
+ "go.mod")
 
 (defun any-file-exists? (files dir)
-  "True if any of the filenames in FILES is in DIR.
-FILES is a list of filenames. DIR is a path to a directory."
+  "True if any of FILES exist in DIR.
+FILES is a list of file names and/or predicates.
+
+An element of FILES can also be a predicate taking one
+argument (a directory) and returning a non-nil value if that
+directory is the one we're looking for.
+
+DIR is a path to a directory."
   (cl-some
-   (lambda (filename) (f-exists? (f-expand filename dir)))
+   (lambda (name)
+     (if (stringp name)
+         (f-exists? (f-expand name dir))
+       (funcall name dir)))
    files))
 
-(defun my-project-root (dir &optional list)
+(defun projectile-root-bottom-up-unprioritized (dir &optional list)
   "Identify a project root.
 Perform a bottom-up search for files in LIST starting from DIR.
 Always return the lowest directory that has any file in LIST. If
 LIST is nil, use `my-project-root-files' instead. Return the
 first (bottommost) matched directory or nil."
   (let ((marker-files (or list my-project-root-files)))
-    (f--traverse-upwards (any-file-exists? marker-files it)
-                         dir)))
+    (f--traverse-upwards
+     (any-file-exists? marker-files it) dir)))
 
 (setq projectile-project-root-functions
       '(projectile-root-local
         projectile-root-bottom-up
-        my-project-root  ;;  Our new function
+        projectile-root-bottom-up-unprioritized  ;;  Our new function
         projectile-root-top-down
         projectile-root-top-down-recurring))
 
