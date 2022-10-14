8fc907136d8e91d2be1dd95bce6ea6803f4f97a4
Chris Clark <cfclrk@gmail.com>
2022-10-14 06:07:09 -0400

    [emacs] Add profile min-markdown-xwidget

---
+++ emacs/min-markdown-xwidget/init.el
@@ -0,0 +1,97 @@
+;;; init.el -- My emacs setup  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'package)
+(setq package-archives
+      '(("gnu" . "http://elpa.gnu.org/packages/") ; http instead of https
+        ("melpa" . "https://melpa.org/packages/")))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+(blink-cursor-mode -1)
+(set-language-environment "UTF-8")
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq-default fill-column 80)
+
+(setq column-number-mode t    ;; Show line:column in mode line
+      help-window-select t
+      inhibit-splash-screen t ;; Do not show the welcome page
+      make-backup-files nil)  ;; Do not save ~ backup files
+
+;; Keep customizations outside of init.el
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file t)
+
+;; ⌘ as Meta and ⎇ as Super on MacOS
+(when (eq system-type 'darwin)
+  (setq mac-command-modifier 'meta
+        mac-option-modifier 'super
+        mac-function-modifier 'hyper)
+
+  ;; Enable emoji
+  (set-fontset-font t 'unicode "Apple Color Emoji" nil 'prepend))
+
+(use-package quelpa)
+
+(unless (package-installed-p 'clojure-mode)
+  (package-install 'clojure-mode))
+
+;;; Packages/Modes
+;;  ----------------------------------------------------------------------------
+
+;;;; ace
+
+(use-package ace-window
+  :bind ("M-l" . ace-window)
+  :config (setq aw-keys '(?a ?o ?e ?u ?h ?t ?n ?s)))
+
+;;;; bicycle
+
+(use-package bicycle
+  :after outline
+  :bind (:map outline-minor-mode-map
+              ([C-tab] . bicycle-cycle)
+              ([S-tab] . bicycle-cycle-global)))
+
+(add-hook 'prog-mode-hook 'outline-minor-mode)
+(add-hook 'prog-mode-hook 'hs-minor-mode)
+
+;;;; rainbow-delimiters
+
+(use-package rainbow-delimiters)
+
+;;;; smartparens
+
+(use-package smartparens
+  :bind (:map lisp-mode-map
+              ("M-f" . sp-next-sexp)
+              ("M-b" . sp-backward-sexp))
+  :init
+  (require 'smartparens-config)
+  :config
+  (load (expand-file-name "~/emacs/smartparens.el"))
+  (my-smartparens-config))
+
+;;;; undo-tree
+
+(use-package undo-tree
+  :config
+  (setq undo-tree-history-directory-alist
+      `((".*" . ,temporary-file-directory)))
+  (setq undo-tree-auto-save-history t)
+  (global-undo-tree-mode))
+
+;;; init.el ends here
