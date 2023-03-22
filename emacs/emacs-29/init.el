5aa4e4f33cbe9441845f4ce069f49e4ffc8e4db1
Chris Clark <cfclrk@gmail.com>
2023-03-22 05:36:16 -0400

    [emacs] Move elpaca setup to its own file

--- emacs/emacs-29/init.el
+++ emacs/emacs-29/init.el
@@ -2,64 +2,21 @@
 
 ;;; Commentary:
 
-;;; Code:
-
-;;; Elpaca
-;;  ----------------------------------------------------------------------------
-
-(defvar elpaca-installer-version 0.3)
-(defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
-(defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
-(defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil
-                              :files (:defaults (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
-       (build (expand-file-name "elpaca/" elpaca-builds-directory))
-       (order (cdr elpaca-order))
-       (default-directory repo))
-  (add-to-list 'load-path (if (file-exists-p build) build repo))
-  (unless (file-exists-p repo)
-    (make-directory repo t)
-    (condition-case-unless-debug err
-        (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                 ((zerop (call-process "git" nil buffer t "clone"
-                                       (plist-get order :repo) repo)))
-                 ((zerop (call-process "git" nil buffer t "checkout"
-                                       (or (plist-get order :ref) "--"))))
-                 (emacs (concat invocation-directory invocation-name))
-                 ((zerop (call-process emacs nil buffer nil "-Q" "-L" "." "--batch"
-                                       "--eval" "(byte-recompile-directory \".\" 0 'force)")))
-                 ((require 'elpaca))
-                 ((elpaca-generate-autoloads "elpaca" repo)))
-            (kill-buffer buffer)
-          (error "%s" (with-current-buffer buffer (buffer-string))))
-      ((error) (warn "%s" err) (delete-directory repo 'recursive))))
-  (unless (require 'elpaca-autoloads nil t)
-    (require 'elpaca)
-    (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
-(add-hook 'after-init-hook #'elpaca-process-queues)
-(elpaca `(,@elpaca-order))
-
-;; Install use-package support
-(elpaca elpaca-use-package
-  (elpaca-use-package-mode)
-  (setq elpaca-use-package-by-default t))
-
-;; Block until current queue processed.
-(elpaca-wait)
-
+;; NOTES:
+;;
 ;; To install a package defined with use-package, evaluate the use-package form
 ;; and then run (elpaca-process-queues).
-
+;;
 ;; TODO:
 ;;
 ;; - precient.el might help with making last-used buffer show up at top of list
 ;;   when switching buffers
 ;; - consult?
 ;; - embark?
+
+;;; Code:
+
+(load (expand-file-name "bootstraps.el" user-emacs-directory))
 
 ;;; Editor General
 ;;  ----------------------------------------------------------------------------
@@ -439,7 +396,7 @@ This prevents duplicates of ENTRY in the alist. Example:
 
 ;;;; markdown
 
-(load (expand-file-name "init-markdown.el" user-emacs-directory))
+(load (expand-file-name "markdown.el" user-emacs-directory))
 
 ;;;; occur
 
