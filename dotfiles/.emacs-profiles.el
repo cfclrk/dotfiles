5b9fa84c66c36c8179ec5a183326e066a9d47bdf
Chris Clark <cfclrk@gmail.com>
2023-10-03 04:36:16 -0400

    [emacs] Add min-elpaca and spacemacs profiles to .emacs-profiles

--- dotfiles/.emacs-profiles.el
+++ dotfiles/.emacs-profiles.el
@@ -3,6 +3,7 @@
  ("emacs-29" . ((user-emacs-directory . "~/emacs/emacs-29")))
  ("cfclrk-emacs-plus" . ((user-emacs-directory . "~/emacs/cfclrk-emacs-plus")))
  ("doom" . ((user-emacs-directory . "~/emacs/doomemacs")))
+ ("min-elpaca" . ((user-emacs-directory . "~/emacs/min-elpaca")))
  ("min-envars" . ((user-emacs-directory . "~/emacs/min-envars")))
  ("min-straight" . ((user-emacs-directory . "~/emacs/min-straight")))
  ("min-package" . ((user-emacs-directory . "~/emacs/min-package")))
@@ -10,4 +11,5 @@
  ("min-clojure" . ((user-emacs-directory . "~/emacs/min-clojure")))
  ("org-clojure" . ((user-emacs-directory . "~/emacs/org-clojure")))
  ("nano" . ((user-emacs-directory . "~/emacs/nano")))
+ ("spacemacs" . ((user-emacs-directory . "~/emacs/spacemacs")))
  ("prelude" . ((user-emacs-directory . "~/emacs/prelude"))))
