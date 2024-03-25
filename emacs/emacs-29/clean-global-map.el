fc1ac7c67188d86b47773ae567009f764c544f4b
Chris Clark <cfclrk@gmail.com>
2024-03-25 15:34:40 -0400

    [emacs] Update global map

--- emacs/emacs-29/clean-global-map.el
+++ emacs/emacs-29/clean-global-map.el
@@ -4,14 +4,17 @@
 
 ;;; Code:
 
-;; duplicates "M-g g" for `goto-line'
+;; Duplicates "M-g g" for `goto-line'
 (keymap-unset global-map "M-g M-g" 'remove)
 
-;; duplicates "M-g n" for `next-error'
+;; Duplicates "M-g n" for `next-error'
 (keymap-unset global-map "M-g M-n" 'remove)
 
-;; duplicates "M-g p" for `previous-error'
+;; Duplicates "M-g p" for `previous-error'
 (keymap-unset global-map "M-g M-p" 'remove)
+
+;; Run's `compose-mail'. I don't use Emacs for email.
+(keymap-unset global-map "C-x m" 'remove)
 
 ;;; clean-global-map.el ends here
 (provide 'clean-global-map)
