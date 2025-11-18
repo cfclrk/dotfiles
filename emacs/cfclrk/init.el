7f66fa71a2f2b5d60eb7d2b660a7a78c899bbe25
Chris Clark <cfclrk@gmail.com>
2025-11-18 09:47:12 -0500

    emacs: Use doom-one-light theme

--- emacs/cfclrk/init.el
+++ emacs/cfclrk/init.el
@@ -149,7 +149,7 @@
   :config
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-acario-light t)
+  (load-theme 'doom-one-light t)
   (doom-themes-visual-bell-config)
   (doom-themes-org-config))
 
