bd7cf6ab91350fa449e0a687342a9fb686b4cb96
Chris Clark <cfclrk@gmail.com>
2026-04-27 10:47:56 -0400

    emacs: Update min-elpaca

--- emacs/min-elpaca/init.el
+++ emacs/min-elpaca/init.el
@@ -7,15 +7,15 @@
 ;;; Elpaca
 ;;  ----------------------------------------------------------------------------
 
-(defvar elpaca-installer-version 0.10)
+(defvar elpaca-installer-version 0.12)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
-(defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
+(defvar elpaca-sources-directory (expand-file-name "sources/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
-                              :build (:not elpaca--activate-package)))
-(let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
+                              :build (:not elpaca-activate)))
+(let* ((repo  (expand-file-name "elpaca/" elpaca-sources-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
        (order (cdr elpaca-order))
        (default-directory repo))
@@ -42,16 +42,9 @@
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
-
-;; Install use-package support
-(elpaca elpaca-use-package
-  ;; Enable :elpaca use-package keyword.
-  (elpaca-use-package-mode)
-  ;; Assume :elpaca t unless otherwise specified.
-  (setq elpaca-use-package-by-default t))
 
 ;; Block until current queue processed.
 (elpaca-wait)
@@ -74,14 +67,7 @@
                     :weight 'normal
                     :height 140)
 
- (use-package completion-preview
-   :ensure nil
-   :hook (prog-mode . completion-preview-mode))
-
 ;;; Packages/Modes
 ;;  ----------------------------------------------------------------------------
-
-(use-package ctrlf
-  :config (ctrlf-mode +1))
 
 ;;; init.el ends here
